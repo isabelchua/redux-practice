64f8a5733f5bf65335e48729f3f5af6301769c67
isabelchua <isabelchua86@gmail.com>
2020-10-18 23:55:02 -0700

    added functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	const counter = useSelector(state => state.counter);
+	const isLogged = useSelector(state => state.isLogged);
+	return (
+		<div className="App">
+			<h1>Counter {counter}</h1>
+
+			{isLogged ? <h3>information</h3> : ""}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore, combineReducers } from "redux";
 import allReducer from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(allReducer);
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById("root")
 );
